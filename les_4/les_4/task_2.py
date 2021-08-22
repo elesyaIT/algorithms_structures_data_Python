b0274a1d96cfc00e6988ce1316bb68435b3f01d4
ivane <ivaneleskin2019@gmail.com>
2021-08-22 22:23:59 +0300

    add hw_4

---
+++ les_4/les_4/task_2.py
@@ -0,0 +1,12 @@
+def test_fib(func):
+    lst = [0,1,1,2,3,5,8,13,21,34]
+    for i, item in enumerate(lst):
+        assert item==func(i)
+        print(f'Test {i} Ok')
+
+def fib(n):
+    if n<2:
+        return n
+    return fib(n-1)+fib(n-2)
+
+test_fib(fib)
